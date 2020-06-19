181f331a527b7d385f3e6398055ed6b12480e91f
Yadhu Krishna <yadhukrrish2@gmail.com>
2020-06-19 12:56:22 +0530

    initial setup client and server side

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Route: /, server.js file');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Backend server started'));
